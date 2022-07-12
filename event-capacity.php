da0175f0ad8f2d6d234db6a1aeb10890afe3deea
AAFaiyaz <abdullaalfaiyaz@gmail.com>
2022-07-12 20:22:30 +0200

    Adding daily coding challenge event capacity

---
+++ event-capacity.php
@@ -0,0 +1,20 @@
+<?php
+
+function check_capacity($capacity, $attendees=0){
+    if($capacity<$attendees){
+        return "This event is sold out ";
+    }else {
+        return "There are still seats on ";
+    }
+}
+
+$vanues = array (
+    'Cantina' => [100,20],
+    'Dorsia' => [98,100],
+    'The Max' => [53,127],
+);
+
+foreach($vanues as $name => $numbers){
+    echo $name . ' - ' .
+    check_capacity($numbers[0], $numbers[1]) . '</br>';
+}
